6900132511a888e8f5ccce0eef236c84bcf56c30
Mark Novicio <Marknovicio@gmail.com>
2018-12-07 20:23:09 -0500

    Updated JS file to add new objects in the Africa array.

---
+++ main.js
@@ -0,0 +1,17 @@
+"use strict";
+
+let imagesAfrica = [
+  {
+    imageNumber: 1,
+    imageDescription: "Swaziland Landscape",
+    image: `src='E:\Personal_Project\Swaziland'`
+  },
+
+  {
+    imageNumber: 2,
+    imageDescription: "Swaziland Landscape",
+    image: `src='E:\Personal_Project\Swaziland_landscape'`
+  }
+];
+
+function nextButton() {}
